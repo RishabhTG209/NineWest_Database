7f2db5b44e0f9112618c7ec4de7fd4f228765020
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-02-24 13:33:26 +0530

    Update productlist.model.js

--- models/productlist.model.js
+++ models/productlist.model.js
@@ -4,10 +4,7 @@ const mongoose = require('mongoose')
 const productListSchema = new mongoose.Schema({
     cat: {type: String, required: true},
     name: {type: String, required: true},
-    image: {type: Number, required: true}
-},{
-    versionKey:false,
-    timestamps:true,
+    image: {type: String, required: true}
 })
 
 module.exports = mongoose.model('productlist',productListSchema)
