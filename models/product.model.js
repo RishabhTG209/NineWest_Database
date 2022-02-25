09bb3a03f81a68fcf1629735a3856249a4dca708
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-02-25 09:16:31 +0530

    Update product.model.js

--- models/product.model.js
+++ models/product.model.js
@@ -4,13 +4,11 @@ const mongoose = require('mongoose')
 const productSchema = new mongoose.Schema({
     pid: {type: Number, required: true},
     name: {type: String, required: true},
-    brand: {type: String, required: true},
     price: {type: Number, required: true},
     discount: {type: Number, required: true},
     description: {type: String, required: true},
     image: [
         {type: String, required: true},
-        {type: String},
         {type: String},
         {type: String}
     ]
