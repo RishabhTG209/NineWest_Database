2c560c921ab46e1cfdd87c008340975eed72ff90
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-02-26 23:55:23 +0530

    changes

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -42,7 +42,7 @@ router.post("/additem/:user_id", async (req,res)=>{
 
             if(req.query.operation=="dec"){
                 await cart.findByIdAndUpdate(cart_item._id,{$inc:{qty:-1}});
-                if(cart_item.qty<=1){
+                if(cart_item.qty<=0 ||cart_item.qty<1){
                     await cart.findByIdAndDelete(cart_item._id);
                 }
             }
